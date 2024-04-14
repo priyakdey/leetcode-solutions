08c191055de6a672760c13197162602c1c93bf97
Priyak Dey <priyak1991dey@gmail.com>
2024-04-15 00:45:43 +0530

    Problem of the day

---
+++ 404.py
@@ -0,0 +1,32 @@
+"""
+404. Sum of Left Leaves
+
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. 
+A left leaf is a leaf that is the left child of another node.
+"""
+
+from typing import Optional
+from model import TreeNode
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 0
+
+        return self._sumOfLeftLeaves(root.left, root) + self._sumOfLeftLeaves(root.right, root)
+
+
+
+    def _sumOfLeftLeaves(self, node: TreeNode, parent: TreeNode, sum=0):
+        if node is None:
+            return 0
+
+        if node.left is None and node.right is None and parent.left == node:
+            return sum + node.val
+
+        return sum + self._sumOfLeftLeaves(node.left, node, sum) + self._sumOfLeftLeaves(node.right, node, sum)
